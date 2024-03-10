6b9dddcd1d7ef20aa0f866a0cc88b07f4363337d
Antonis Sykoutris <antonis.sykoutris@protonmail.com>
2024-03-10 20:45:09 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,19 @@
-'use client'
-import { useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { Environment, Scroll, ScrollControls } from '@react-three/drei'
-import { MotionConfig } from 'framer-motion'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
-import { framerMotionConfig } from '@/lib/utils'
-import FirstSection from '@/components/FirstSection/FirstSection'
-import SecondSection from '@/components/SecondSection/SecondSection'
-import ThirdSection from '@/components/ThirdSection/ThirdSection'
-// import { ScrollManager } from '@/components/General/ScrollManager'
+'use client';
+import { MotionConfig } from 'framer-motion';
 
-gsap.registerPlugin(ScrollTrigger)
+import { framerMotionConfig } from '@/lib/utils';
+import Hero from '@/components/Hero/Hero';
+import About from '@/components/About/About';
+
 export default function Home() {
-  // const [section, setSection] = useState(0)
   return (
-    <section className='h-screen w-full'>
-      <MotionConfig
-        transition={{
-          ...framerMotionConfig
-        }}
-      >
-        <Canvas camera={{ position: [1, 1, 5], near: 0.01 }}>
-          <Environment preset='city' />
-          <ScrollControls pages={3} damping={0.25}>
-            {/* <ScrollManager section={section} onSectionChange={setSection} /> Uncomment if you desire section snap on scroll */}
-            {/* Add your 3D OBJECT HERE */}
-            <Scroll html style={{ width: '100%' }}>
-              <FirstSection />
-              <SecondSection />
-              <ThirdSection />
-            </Scroll>
-          </ScrollControls>
-        </Canvas>
-      </MotionConfig>
-    </section>
-  )
+    <MotionConfig
+      transition={{
+        ...framerMotionConfig
+      }}
+    >
+      <Hero />
+      <About />
+    </MotionConfig>
+  );
 }
